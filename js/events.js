1401c49330bd4cd50c1dbfdb9fcf4ac586abc3c8
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-22 19:49:31 +0600

    commited

--- js/events.js
+++ js/events.js
@@ -2,3 +2,39 @@ console.log('hello')
 function makeRed(){
     document.body.style.backgroundColor = 'red';
 }
+//option 1
+//<button onclick="console.log(19)">another me</button>
+//option 2
+//    <button onclick="makeRed()">make red</button>
+
+
+//option 3
+const makePurpleButton = document.getElementById('make-purple');
+makePurpleButton.onclick = makePurple;
+
+function makePurple(){
+  document.body.style.backgroundColor = 'purple'; 
+}
+
+//option 4
+
+const makeGreenButton = document.getElementById('make-green');
+     makeGreenButton.onclick =function makeGreen(){
+        document.body.style.backgroundColor = 'green';
+     }
+
+
+//option 5
+const makePinkButtons = document.getElementById('make-pink')
+makePinkButtons.addEventListener('click', makePink)
+function makePink (){
+    document.body.style.backgroundColor = 'pink';
+}
+
+
+
+
+
+
+
+
